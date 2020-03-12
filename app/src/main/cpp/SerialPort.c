44c1fdb41f8321772dbb530007089228bb5894d0
安德鲁 <andrew@zhuitech.com>
2020-03-12 20:48:00 +0800

    添加校验位，停止位

--- app/src/main/cpp/SerialPort.c
+++ app/src/main/cpp/SerialPort.c
@@ -26,46 +26,55 @@
 
 #include "android/log.h"
 static const char *TAG="serial_port";
-#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
-#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
-#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
 
 static speed_t getBaudrate(jint baudrate)
 {
 	switch(baudrate) {
-	case 0: return B0;
-	case 50: return B50;
-	case 75: return B75;
-	case 110: return B110;
-	case 134: return B134;
-	case 150: return B150;
-	case 200: return B200;
-	case 300: return B300;
-	case 600: return B600;
-	case 1200: return B1200;
-	case 1800: return B1800;
-	case 2400: return B2400;
-	case 4800: return B4800;
-	case 9600: return B9600;
-	case 19200: return B19200;
-	case 38400: return B38400;
-	case 57600: return B57600;
-	case 115200: return B115200;
-	case 230400: return B230400;
-	case 460800: return B460800;
-	case 500000: return B500000;
-	case 576000: return B576000;
-	case 921600: return B921600;
-	case 1000000: return B1000000;
-	case 1152000: return B1152000;
-	case 1500000: return B1500000;
-	case 2000000: return B2000000;
-	case 2500000: return B2500000;
-	case 3000000: return B3000000;
-	case 3500000: return B3500000;
-	case 4000000: return B4000000;
-	default: return -1;
+		case 0: return B0;
+		case 50: return B50;
+		case 75: return B75;
+		case 110: return B110;
+		case 134: return B134;
+		case 150: return B150;
+		case 200: return B200;
+		case 300: return B300;
+		case 600: return B600;
+		case 1200: return B1200;
+		case 1800: return B1800;
+		case 2400: return B2400;
+		case 4800: return B4800;
+		case 9600: return B9600;
+		case 19200: return B19200;
+		case 38400: return B38400;
+		case 57600: return B57600;
+		case 115200: return B115200;
+		case 230400: return B230400;
+		case 460800: return B460800;
+		case 500000: return B500000;
+		case 576000: return B576000;
+		case 921600: return B921600;
+		case 1000000: return B1000000;
+		case 1152000: return B1152000;
+		case 1500000: return B1500000;
+		case 2000000: return B2000000;
+		case 2500000: return B2500000;
+		case 3000000: return B3000000;
+		case 3500000: return B3500000;
+		case 4000000: return B4000000;
+		default: return -1;
 	}
+}
+
+static void throwException(JNIEnv *env, const char *name, const char *msg)
+{
+	jclass cls = (*env)->FindClass(env, name);
+	/* if cls is NULL, an exception has already been thrown */
+	if (cls != NULL) {
+		(*env)->ThrowNew(env, cls, msg);
+	}
+
+	/* free the local ref */
+	(*env)->DeleteLocalRef(env, cls);
 }
 
 /*
@@ -74,18 +83,32 @@ static speed_t getBaudrate(jint baudrate)
  * Signature: (Ljava/lang/String;II)Ljava/io/FileDescriptor;
  */
 JNIEXPORT jobject JNICALL Java_android_1serialport_1api_SerialPort_open
-  (JNIEnv *env, jclass thiz, jstring path, jint baudrate, jint flags)
+		(JNIEnv *env, jclass thiz, jstring path, jint baudrate, jint parity, jint dataBits, jint stopBit)
 {
 	int fd;
+	int flags;
 	speed_t speed;
 	jobject mFileDescriptor;
+
+	flags=0;
 
 	/* Check arguments */
 	{
 		speed = getBaudrate(baudrate);
 		if (speed == -1) {
-			/* TODO: throw an exception */
-			LOGE("Invalid baudrate");
+			throwException(env, "java/lang/IllegalArgumentException", "Invalid baudrate");
+			return NULL;
+		}
+		if (parity <0 || parity>2) {
+			throwException(env, "java/lang/IllegalArgumentException", "Invalid parity");
+			return NULL;
+		}
+		if (dataBits <5 || dataBits>8) {
+			throwException(env, "java/lang/IllegalArgumentException", "Invalid dataBits");
+			return NULL;
+		}
+		if (stopBit <1 || stopBit>2) {
+			throwException(env, "java/lang/IllegalArgumentException", "Invalid stopBit");
 			return NULL;
 		}
 	}
@@ -94,15 +117,11 @@ JNIEXPORT jobject JNICALL Java_android_1serialport_1api_SerialPort_open
 	{
 		jboolean iscopy;
 		const char *path_utf = (*env)->GetStringUTFChars(env, path, &iscopy);
-		LOGD("Opening serial port %s with flags 0x%x", path_utf, O_RDWR | flags);
 		fd = open(path_utf, O_RDWR | flags);
-		LOGD("open() fd = %d", fd);
 		(*env)->ReleaseStringUTFChars(env, path, path_utf);
 		if (fd == -1)
 		{
-			/* Throw an exception */
-			LOGE("Cannot open port");
-			/* TODO: throw an exception */
+			throwException(env, "java/io/IOException", "Cannot open port");
 			return NULL;
 		}
 	}
@@ -110,12 +129,10 @@ JNIEXPORT jobject JNICALL Java_android_1serialport_1api_SerialPort_open
 	/* Configure device */
 	{
 		struct termios cfg;
-		LOGD("Configuring serial port");
 		if (tcgetattr(fd, &cfg))
 		{
-			LOGE("tcgetattr() failed");
 			close(fd);
-			/* TODO: throw an exception */
+			throwException(env, "java/io/IOException", "tcgetattr() failed");
 			return NULL;
 		}
 
@@ -123,13 +140,31 @@ JNIEXPORT jobject JNICALL Java_android_1serialport_1api_SerialPort_open
 		cfsetispeed(&cfg, speed);
 		cfsetospeed(&cfg, speed);
 
-		if (tcsetattr(fd, TCSANOW, &cfg))
-		{
-			LOGE("tcsetattr() failed");
-			close(fd);
-			/* TODO: throw an exception */
-			return NULL;
+		/* More attribute set */
+		switch (parity) {
+			case 0: break;
+			case 1: cfg.c_cflag |= PARENB; break;
+			case 2: cfg.c_cflag &= ~PARODD; break;
 		}
+		switch (dataBits) {
+			case 5: cfg.c_cflag |= CS5; break;
+			case 6: cfg.c_cflag |= CS6; break;
+			case 7: cfg.c_cflag |= CS7; break;
+			case 8: cfg.c_cflag |= CS8; break;
+		}
+		switch (stopBit) {
+			case 1: cfg.c_cflag &= ~CSTOPB; break;
+			case 2: cfg.c_cflag |= CSTOPB; break;
+		}
+		int rc = tcsetattr(fd, TCSANOW, &cfg);
+		/*
+        if (rc)
+        {
+            close(fd);
+            throwException(env, "java/io/IOException", strcat("tcsetattr() failed: ", rc));
+            return NULL;
+        }
+        */
 	}
 
 	/* Create a corresponding file descriptor */
@@ -161,7 +196,5 @@ JNIEXPORT void JNICALL Java_android_1serialport_1api_SerialPort_close
 	jobject mFd = (*env)->GetObjectField(env, thiz, mFdID);
 	jint descriptor = (*env)->GetIntField(env, mFd, descriptorID);
 
-	LOGD("close(fd = %d)", descriptor);
 	close(descriptor);
 }
-
